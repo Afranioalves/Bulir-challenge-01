971687c23dceed345a1e155e6731bc77762c7259
Afranioalves <afranio777alves@gmail.com>
2024-07-23 13:45:29 +0100

    refactor: mudar de nome do ficheiro

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,9 @@
+import Models from "../models";
+import { userInput } from "../interfaces/user.interface";
+
+const create = async (user:userInput) => await Models.users.create(user);
+const findUserByEmail = async (email: string) =>await Models.users.findOne({ where: { email } });
+const findUserByNIF = async (NIF: string) =>await Models.users.findOne({ where: { NIF } });
+
+const userRepositoriy = { create, findUserByEmail, findUserByNIF}
+export default userRepositoriy
