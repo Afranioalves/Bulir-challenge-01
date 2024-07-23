65870e366d991057d305c6f90d3458ccff4195bb
Afranioalves <afranio777alves@gmail.com>
2024-07-23 10:24:48 +0100

    chore: criar model de usuario

---
+++ src/models/user.model.ts
@@ -0,0 +1,35 @@
+import Sequelize from "sequelize";
+import database from "../config/database";
+import { userSchema } from "../interfaces/user.interface";
+
+const users = database.define<userSchema>("users",{
+    id:{
+        type:Sequelize.STRING(400), 
+        autoIncrement: false,
+        allowNull: false,
+        primaryKey:true,
+    },
+    fullName:{
+        type:Sequelize.STRING(400),
+        allowNull: false, 
+    },
+    email:{
+        type:Sequelize.STRING(200),
+        allowNull: false, 
+    },
+    NIF:{
+        type:Sequelize.STRING(50),
+        allowNull: true, 
+    },
+    password:{
+        type:Sequelize.STRING(400),
+        allowNull: true, 
+    },
+    userType:{
+        type: Sequelize.ENUM('PRESTADOR','CLIENTE'),
+        allowNull:true,
+    }
+})
+
+
+export default users
