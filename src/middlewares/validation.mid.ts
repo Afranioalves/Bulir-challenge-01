68a658400262117104674103197d7cbbb43b3abc
Afranioalves <afranio777alves@gmail.com>
2024-07-23 13:39:58 +0100

    chore: adicionar middlewares de validação

---
+++ src/middlewares/validation.mid.ts
@@ -0,0 +1,11 @@
+import {check} from "express-validator"
+
+
+export const createUserValidation = [
+    check('fullName').not().isEmpty().trim().escape().withMessage('campo fullName obrigatório'),
+    check('NIF').not().isEmpty().trim().escape().withMessage('campo NIF obrigatório'),
+    check('email').not().isEmpty().trim().escape().withMessage('campo email obrigatório'),
+    check('password').not().isEmpty().trim().escape().withMessage('campo password obrigatório'),
+    check('userType').not().isEmpty().trim().escape().withMessage('campo userType obrigatório'),
+    
+]
