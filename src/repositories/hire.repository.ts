c297b4aa5f2ff6383d331cd50b236c3e8d04a66c
Afranioalves <afranio777alves@gmail.com>
2024-07-24 20:18:39 +0100

    feat: adicionar metodo para ver serviços

--- src/repositories/hire.repository.ts
+++ src/repositories/hire.repository.ts
@@ -1,10 +1,20 @@
 import Models from "../models";
 import { hireInput } from '../interfaces/hire.interface';
+import { Op } from "sequelize";
 
 
-const create = async (transaction:hireInput) => await Models.hires.create(transaction);
-const findAll = async () => await Models.hires.findAll();
+const create = async (hiring:hireInput) => await Models.hires.create(hiring);
 
-const transactionRepository = { create, findAll };
+const findAll = async (userId:string) => await Models.hires.findAll({
+    where :{[Op.or]:[{providerId:userId},{costumerId: userId}]},
+    attributes:['id','amount','createdAt'],
+    include: [
+    { model: Models.services, as: 'service', attributes:['title']},
+    { model: Models.users, as:'provider', attributes:['fullName']},
+    { model: Models.users, as:'costumer', attributes:['fullName']},
+  
+  ]});
 
-export default transactionRepository
+const hireRepository = { create, findAll };
+
+export default hireRepository
