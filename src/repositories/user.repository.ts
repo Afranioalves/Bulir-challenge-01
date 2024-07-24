28129df13828db1d567f349cb3089c6f7e6c22ba
Afranioalves <afranio777alves@gmail.com>
2024-07-24 11:33:44 +0100

    feat: adicionar metodos para buscar usuario pelo id

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -4,6 +4,7 @@ import { userInput } from "../interfaces/user.interface";
 const create = async (user:userInput) => await Models.users.create(user);
 const findUserByEmail = async (email: string) =>await Models.users.findOne({ where: { email } });
 const findUserByNIF = async (NIF: string) =>await Models.users.findOne({ where: { NIF } });
+const findUserById = async (id: string) =>await Models.users.findByPk(id);
 
-const userRepository = { create, findUserByEmail, findUserByNIF}
+const userRepository = { create, findUserByEmail, findUserByNIF, findUserById}
 export default userRepository
