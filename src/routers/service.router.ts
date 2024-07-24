ca9e25279f0a2f26254d6245b3f6ee1b0040cf67
Afranioalves <afranio777alves@gmail.com>
2024-07-24 11:35:05 +0100

    chore: adicionar routa para buscar usuario

--- src/routers/service.router.ts
+++ src/routers/service.router.ts
@@ -4,9 +4,10 @@ import { createServiceValidation } from "../middlewares/validation.mid";
 import { Authorization } from "../middlewares/auth.mid";
 const serviceRouter = express.Router();
 
-const { create } = serviceController
+const { create, findAll } = serviceController
 
 serviceRouter.post("/", Authorization, createServiceValidation, create);
+serviceRouter.get("/", Authorization, findAll);
 
 
 export default serviceRouter; 
