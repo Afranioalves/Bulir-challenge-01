c3762ac0a99ad797811fa39b7334e0f4fb06b7d8
Afranioalves <afranio777alves@gmail.com>
2024-07-24 20:19:01 +0100

    feat: adicionar metodo para ver serviços

--- src/repositories/service.repository.ts
+++ src/repositories/service.repository.ts
@@ -4,7 +4,8 @@ import { serviceInput } from "../interfaces/service.interface";
 const create = async (service:serviceInput) => await Models.services.create(service);
 const findAll = async () => await Models.services.findAll({where:{status:true}, include:Models.users});
 const findServiceById = async (id:string) => await Models.services.findByPk(id);
+const findServiceByOnwnerId = async (ownerId:string) => await Models.services.findAll({where:{ownerId}});
 
-const serviceRepository = { create, findAll, findServiceById}
+const serviceRepository = { create, findAll, findServiceById, findServiceByOnwnerId}
 
 export default serviceRepository
