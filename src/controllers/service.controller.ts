67bd8e0df03838d3c7ef8a167fe3e4e456517929
Afranioalves <afranio777alves@gmail.com>
2024-07-24 13:38:30 +0100

    feat: adicionar metodo para actualizar serviço

--- src/controllers/service.controller.ts
+++ src/controllers/service.controller.ts
@@ -4,6 +4,7 @@ import { validationResult } from "express-validator";
 import { serviceInput } from "../interfaces/service.interface";
 import serviceRepository from "../repositories/service.repository";
 import { serviceOrderOutput } from "../utils/order";
+import { numberValidator } from "../libs/number-validator";
 
 const create = async (req: Request, res: Response) => {
 
@@ -17,7 +18,8 @@ const create = async (req: Request, res: Response) => {
         const {id, userType} = req.body.user;
         
         if(userType != "PRESTADOR") return res.status(401).send({error:'Sem permisão', message:'Actualize a sua conta para prestador'})
-       
+        if(!numberValidator(price)) return res.status(400).send({ error: "Preço invalido", message: "Insira um preço valido para esse serviço" });
+        
         const service: serviceInput = {
             id: serviceId,
             title,
@@ -79,8 +81,46 @@ const enableAndDisableService = async (req: Request, res: Response) => {
 }
 
 
+const update = async (req: Request, res: Response) => {
 
-const serviceController = { create, findAll, enableAndDisableService }
+    try {
+        const error = validationResult(req);
+        if (!error.isEmpty()) return res.status(400).send({ message: error.array() });
+
+        const {title, description, price} = req.body
+        const {serviceId} = req.params
+        const {id, userType} = req.body.user;
+
+        const resultService = await serviceRepository.findServiceById(serviceId)
+        if(resultService == null) return res.status(404).send({ error: "Serviço não encontrado", message: "Serviço que pretende actualizar, não existe" });
+        if(!numberValidator(price)) return res.status(400).send({ error: "Preço invalido", message: "Insira um preço valido para esse serviço" });
+        if(resultService.ownerId != id) return res.status(401).send({ error: "Sem permissão", message: "Não tens permissão para actualizar este serviço" });
+        const service =  resultService;
+        service.title = title;
+        service.description = description;
+        service.price = price;
+        await service.save();
+        return res.status(200).send({ 
+            message:'Serviço actualizado com sucesso', 
+            service:{
+               id:service.id,
+               title,
+               description,
+               price,
+            }
+        })
+    
+     
+    } catch (error) {
+        console.error('Erro no processamento:', error);
+        res.status(500).send({error, message:'Erro ao excluir serviços' });
+    }
+
+}
+
+
+
+const serviceController = { create, findAll, enableAndDisableService, update }
 export default serviceController
 
 
