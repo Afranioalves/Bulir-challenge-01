0e80df3cb5f3cf7d907448e72d6c971af7aa66ca
Afranioalves <afranio777alves@gmail.com>
2024-07-24 13:39:14 +0100

    chore: adicionar routa para actualizar serviço

--- src/routers/service.router.ts
+++ src/routers/service.router.ts
@@ -4,10 +4,11 @@ import { createServiceValidation } from "../middlewares/validation.mid";
 import { Authorization } from "../middlewares/auth.mid";
 const serviceRouter = express.Router();
 
-const { create, findAll, enableAndDisableService} = serviceController
+const { create, findAll, enableAndDisableService, update} = serviceController
 
 serviceRouter.post("/", Authorization, createServiceValidation, create);
 serviceRouter.get("/", Authorization, findAll);
 serviceRouter.put("/:serviceId", Authorization,  enableAndDisableService);
+serviceRouter.put("/update/:serviceId", Authorization, createServiceValidation, update);
 
 export default serviceRouter; 
