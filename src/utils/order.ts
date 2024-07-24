f67e02086693ca396481fa1ffeb00150a5dfeee3
Afranioalves <afranio777alves@gmail.com>
2024-07-24 20:12:01 +0100

    chore: adicionar novos metodos de organizar dados

--- src/utils/order.ts
+++ src/utils/order.ts
@@ -14,3 +14,33 @@ export const serviceOrderOutput = (data:Array<object>) =>{
         }
     })
 }
+
+
+
+export const hiringOrderOutput = (data:Array<object>) =>{
+    return data.map((hire:any)=>{
+        return {
+            id: hire.id,
+            service: hire.service.title,
+            amount: hire.amount,
+            costumer: hire.costumer.fullName,
+            provider: hire.provider.fullName,
+            hiringDate: hire.createdAt,
+        }
+    })
+}
+
+
+export const serviceOnwerOrderOutput = (data:Array<object>) =>{
+    return data.map((service:any)=>{
+        return {
+            id: service.id,
+            title: service.title,
+            description: service.description,
+            price: service.price,
+            status: service.status ? 'ACTIVADO' :'DESACTIVADO',
+            createdAt:service.createdAt,
+           
+        }
+    })
+}
