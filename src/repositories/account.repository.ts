40ffb211a2e1713d6de8f475448a7ac893963c96
Afranioalves <afranio777alves@gmail.com>
2024-07-24 11:31:43 +0100

    feat: adicionar metodos para buscar balance

--- src/repositories/account.repository.ts
+++ src/repositories/account.repository.ts
@@ -2,6 +2,7 @@ import Models from "../models";
 import { accountInput } from "../interfaces/account.interface";
 
 const create = async (account:accountInput) => await Models.accounts.create(account);
+const findAcountByOwnerId = async (ownerId: string) =>await Models.accounts.findOne({ where: { ownerId } });
 
-const accountRepository = { create }
+const accountRepository = { create, findAcountByOwnerId }
 export default accountRepository
